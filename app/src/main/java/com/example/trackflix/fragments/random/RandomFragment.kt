c7d644e0da0f62f0c51874b67d5667854d84fdbb
PhOberd <phoberdorfer@edu.aau.at>
2023-07-15 15:14:08 +0200

    Implemented random screen suggestion functionality

--- app/src/main/java/com/example/trackflix/fragments/random/RandomFragment.kt
+++ app/src/main/java/com/example/trackflix/fragments/random/RandomFragment.kt
@@ -1,16 +1,23 @@
 package com.example.trackflix.fragments.random
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.trackflix.R
+import com.example.trackflix.databinding.FragmentRandomBinding
+import com.example.trackflix.fragments.list.ListAdapter
+import com.example.trackflix.model.Trackable
+import com.example.trackflix.viewModel.TrackableViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,16 +25,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RandomFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var myTrackableViewModel: TrackableViewModel
+    private lateinit var binding: FragmentRandomBinding
+    private val args by navArgs<RandomFragmentArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +38,61 @@ class RandomFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_random, container, false)
+        binding = FragmentRandomBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
+
+        init()
+
+        binding.backBtn.setOnClickListener{
+            findNavController().navigate((R.id.action_randomFragment_to_listFragment))
+        }
+
+        binding.rerollBtn.setOnClickListener{
+            init()
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RandomFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RandomFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun init(){
+        val filteredTrackables = args.trackableList.trackables.filter{trackable ->
+            trackable.progressState == "inProgress"
+        }
+        val randTrackable = filteredTrackables.random()
+
+        binding.titleTV.text = randTrackable.title
+        binding.consumedTV.text = randTrackable.currentProgress.toString()
+        binding.goalTV.text = randTrackable.goal.toString()
+        binding.diffrenceTV.text = (randTrackable.goal-randTrackable.currentProgress).toString()
+
+        if(randTrackable.type == "Book"){
+            binding.diffTypeTV.setText(R.string.sides)
+            binding.goalTypeTV.setText(R.string.sides)
+            binding.typeTV.setText(R.string.sides)
+        }else{
+            binding.diffTypeTV.setText(R.string.hours)
+            binding.goalTypeTV.setText(R.string.hours)
+            binding.typeTV.setText(R.string.hours)
+        }
     }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment RandomFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            RandomFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
